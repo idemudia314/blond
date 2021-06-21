920fc279f37725bfa805fe338fc37765507606d7
David Godwin <david@Davids-MacBook-Air.local>
2021-06-21 05:44:10 -0700

    django templates

---
+++ the_blond/blond/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import Post
+
+
+# Create your views here.
+
+
+def index(request):
+    posts = Post.objects.all()
+
+    context = {
+        "posts": posts
+    }
+
+
+    return render(request, "blond/index.html", context )
